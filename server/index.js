16bc90ab67933e6230485bb6c65f07c505fbf72c
peterjbone <peterjbone41@gmail.com>
2024-10-30 18:39:47 -0500

    creating the models in mongoose

--- server/index.js
+++ server/index.js
@@ -1,14 +1,22 @@
+require("dotenv").config();
 const express = require("express");
 const server = express();
 const cors = require("cors");
-require("dotenv").config();
-
 const { PORT } = process.env || 5000;
+const mongoose = require("mongoose");
 
 //* middlewares
 server.use(cors());
 server.use(express.json());
 
-server.listen(PORT, () => {
-	console.log(`Server raised at port ${PORT}`);
+server.listen(PORT, async () => {
+	try {
+		console.log(`Server raised at port ${PORT}`);
+
+		await mongoose.connect(process.env.DB_URL);
+
+		console.log("DB Connected Successfully!");
+	} catch (error) {
+		console.log(error);
+	}
 });
